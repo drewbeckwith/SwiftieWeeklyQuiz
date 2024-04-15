b8fc873e7b7cdbe9268783a562e51e9bac106a6e
drewb <drew.beckwith@mcleodsoftware.com>
2024-04-15 16:58:32 -0500

    leaderboard work

--- mvp-starter/components/admin.js
+++ mvp-starter/components/admin.js
@@ -5,11 +5,6 @@ import { app, db } from '../firebase/firebase.js';
 import { collection, getDocs, getDoc, updateDoc, doc, setDoc } from "firebase/firestore";
 import { useAuth } from '../firebase/auth';
 //TODO 
-//3. admin roles
-//4. test mode
-//5. Fixed height
-//6. leaderboard scrollable
-//7. style sign in page
 
 const Admin = ({questions}) => {
     console.log(questions);
